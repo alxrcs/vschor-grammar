10ae687c4ffe04831487989bae52fd1c55b2d662
Alex Coto <alexrc09@gmail.com>
2023-05-06 01:26:59 +0200

    Add error parsing for GC projection

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,17 @@
 import * as vscode from 'vscode';
 
-import { execFile } from 'child_process';
+import * as cp from 'child_process';
 import * as util from 'util';
 import * as path from 'path';
+import assert = require('assert');
+
 import { getGenerateLTSCommand, getProjectCommand, getPreviewCommand } from './config';
 
+const diagnosticCollection = vscode.languages.createDiagnosticCollection('vschor-grammar');
+
 export function activate(context: vscode.ExtensionContext) {
-	// Register a command to generate the LTS of the 
+
+	// Register a command to generate the LTS of the currently open FSA
 	context.subscriptions.push(vscode.commands.registerCommand(
 		"vschor-grammar.genlts",
 		generateLTSCommand
@@ -15,6 +20,12 @@ export function activate(context: vscode.ExtensionContext) {
 		"vschor-grammar.project",
 		projectGC
 	))
+
+	context.subscriptions.push(
+		vscode.workspace.onDidCloseTextDocument(document => {
+			diagnosticCollection.delete(document.uri);
+		})
+	);
 
 	// Generate previews whether opening or saving the original files
 	vscode.workspace.onDidSaveTextDocument(generatePreview);
@@ -56,11 +67,8 @@ async function projectGC() {
 		title: "Projecting GC",
 		cancellable: false
 	}, async (progress, token) => {
-		if (!vscode.workspace.workspaceFolders ||
-			vscode.workspace.workspaceFolders?.length < 1) {
-			vscode.window.showErrorMessage("ChorGram folder must be open.");
-			return;
-		} else if (vscode.window.activeTextEditor?.document.languageId != "gc") {
+
+		if (vscode.window.activeTextEditor?.document.languageId != "gc") {
 			vscode.window.showErrorMessage("Please select a valid gc file.");
 			return;
 		}
@@ -72,66 +80,109 @@ async function projectGC() {
 		// Open the resulting fsa file
 		let uri = vscode.Uri.file(
 			path.join(
-				path.join(path.dirname(filename), "fsa"), path.basename(filename, ".gc") + ".fsa")
+				path.dirname(filename),
+				path.basename(filename, ".gc") + ".fsa"
+			)
 		);
 		await vscode.commands.executeCommand('vscode.open', uri);
 	});
 }
 
 async function generatePreview(document: vscode.TextDocument): Promise<void> {
-
 	if (!(["gc", "fsa"].includes(document.languageId))) return;
 
-	if (!vscode.workspace.workspaceFolders ||
-		vscode.workspace.workspaceFolders?.length < 1) {
-		vscode.window.showErrorMessage("ChorGram folder must be open.");
-		// TODO: Make chorgram executables installable & configurable in the extension.
-		return;
-	}
-
 	let cmd = getPreviewCommand(document.languageId)
-	const dot = await runCommand(cmd, document.uri.path)
+	const dot = await runCommand(cmd, document.uri.fsPath)
+
+	assert(vscode.workspace.workspaceFolders, "No workspace folder found")
+	let folder = vscode.workspace.workspaceFolders[0].uri.fsPath;
 
 	// Write the command output to a tmp file
 	const encoder = new util.TextEncoder(); // UTF-8 by default
-	const tmpDotURI = vscode.Uri.file(path.join(vscode.workspace.workspaceFolders[0].uri.path, document.fileName + '.tmp.dot'));
+	const tmpDotURI = vscode.Uri.file(document.fileName + '.tmp.dot');
+
 	await vscode.workspace.fs.writeFile(tmpDotURI, encoder.encode(dot))
 
 	// Show DOT preview
 	await vscode.commands.executeCommand('graphviz.previewToSide', tmpDotURI);
 
-	// Return focus to the original document
-	await vscode.window.showTextDocument(document, vscode.ViewColumn.One);
+	// Transform the dot file into a pdf
+	const pdf = await runCommand(`dot -Tpdf ${tmpDotURI.path}`, folder)
+
+	// Write the pdf to a tmp file
+	const tmpPDFURI = vscode.Uri.file(
+		path.join(folder, document.fileName + '.pdf')
+	);
+
+	await vscode.workspace.fs.writeFile(tmpPDFURI, encoder.encode(pdf))
+
+	// Return control back to the original editor
+	await vscode.window.showTextDocument(document, vscode.ViewColumn.One, false);
 }
 
-async function runCommand(cmd: string, path: string) {
 
-	if (!vscode.workspace.workspaceFolders ||
-		vscode.workspace.workspaceFolders?.length < 1) {
-		vscode.window.showErrorMessage("ChorGram folder must be open.");
-		// TODO: Make chorgram executables installable & configurable in the extension.
-		return;
+/**
+ * Run the given command in the given path.
+ * @param cmd - The command to run
+ * @param path - The path to run the command in
+ * @returns - A Promise that resolves with the command output or rejects with an error
+ */
+async function runCommand(cmd: string, path: string): Promise<string> {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (!workspaceFolders) {
+		vscode.window.showErrorMessage("No workspace folder found");
+		return "";
 	}
 
-	const cwd = vscode.workspace.workspaceFolders[0];
-	let fullCmd = cwd.uri.path + '/' + cmd;
+	const cwd = workspaceFolders[0].uri.fsPath;
+	const document = vscode.window.activeTextEditor?.document as vscode.TextDocument;
 
-	return new Promise<string>((resolve, reject) => {
-		execFile(
-			cmd[0] == '.' ? fullCmd : cmd,
-			[path],
-			{ cwd: cwd.uri.path, shell: '/bin/bash', env: process.env },
-			async (error, stdout, stderr) => {
-				if (error) {
-					reject(stderr);
-					await vscode.window.showErrorMessage(error.message);
-					await vscode.window.showErrorMessage(stderr);
-					return;
+	try {
+		return new Promise<string>((resolve, reject) => {
+			cp.execFile(
+				cmd,
+				[path],
+				{ cwd, shell: '/bin/bash', env: process.env },
+				async (error, stdout, stderr) => {
+					if (error) {
+						reject(stderr);
+						// await vscode.window.showErrorMessage(error.message);
+						let errors = parseErrors(stderr, document);
+						diagnosticCollection.set(document.uri, errors);
+
+						// await vscode.window.showErrorMessage(errors[0].message);
+						return;
+					} else {
+						diagnosticCollection.set(document.uri, [])
+					}
+					resolve(stdout.trim());
 				}
-				resolve(stdout.trim());
-			}
-		);
-	})
+			);
+		});
+	} catch (error: any) {
+		vscode.window.showErrorMessage(error.message);
+		return "";
+	}
+}
+
+function parseErrors(errorOutput: string, document: vscode.TextDocument): vscode.Diagnostic[] {
+	const diagnostics: vscode.Diagnostic[] = [];
+
+	// Regular expression to match error messages
+	const errorRegex = /Syntax error at <(\d+),(\d+)>: (.*)(?:\n|$)/g;
+	let match: RegExpExecArray | null;
+
+	while ((match = errorRegex.exec(errorOutput)) !== null) {
+		const lineNumber = parseInt(match[1], 10) - 1; // 0-based line number
+		const columnNumber = parseInt(match[2], 10) - 1; // 0-based column number
+		const errorMessage = match[3];
+
+		const range = new vscode.Range(lineNumber, columnNumber, lineNumber, columnNumber + 1);
+		const diagnostic = new vscode.Diagnostic(range, errorMessage, vscode.DiagnosticSeverity.Error);
+		diagnostics.push(diagnostic);
+	}
+
+	return diagnostics;
 }
 
 
